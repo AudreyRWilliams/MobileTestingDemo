b5d6ff82e5f4d781b0c0d9c92a85fb7acb4b23f2
Audrey Williams <120662264+AudreyRWilliams@users.noreply.github.com>
2025-08-26 22:01:08 -0400

    Update ViewController.swift

--- ios/HelloSim/ViewController.swift
+++ ios/HelloSim/ViewController.swift
@@ -1,1 +1,15 @@
 
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var helloLabel: UILabel!
+
+    @IBAction func didTap(_ sender: UIButton) {
+        helloLabel.text = "Tapped!"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
